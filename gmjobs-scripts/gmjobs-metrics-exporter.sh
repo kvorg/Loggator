2ca3dfd20973f2ef30b85c2fe6b8b75d828e0bcf
Jan Jona Javorsek <jona@udatt.(none)>
2010-08-27 15:10:31 +0200

    More blahmode support

--- gmjobs-scripts/gmjobs-metrics-exporter.sh
+++ gmjobs-scripts/gmjobs-metrics-exporter.sh
@@ -1,2 +1,2 @@
 cd /opt/gmjobs-metrics-export 
-./gmjobs-metrics-exporter -c /opt/gmjobs-metrics-export/gmjobs-metrics-exporter.rc/ -l /var/log/gmjobs-metrics-exporter.log -s /var/state/gmjobs-metrics-exporter.state -u /var/state/gmjobs-metrics-exporter/gmjobs-metrics-exporter.undo-%d-%t.sql > /dev/null 2>&1
+./gmjobs-metrics-exporter -B -c /opt/gmjobs-metrics-export/gmjobs-metrics-exporter.rc/ -l /var/log/gmjobs-metrics-exporter.log -s /var/state/gmjobs-metrics-exporter.state -u /var/state/gmjobs-metrics-exporter/gmjobs-metrics-exporter.undo-%d-%t.sql > /dev/null 2>&1
